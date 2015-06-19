648fdb8fd994cb93aeca531ba8746c90c5d99bc7
Shyla Punteney <shyspark@shyspark.com>
2015-06-18 20:09:54 -0500

    more tweaks5

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-    $('button').click(function() {
+    $('#b1').click(function() {
         $('#red').fadeOut('slow');
     });
+       $('#tree').mouseenter(function() {
+       $(this).fadeOut('slow');
+
+    }); 
+           $('#b2').click(function() {
+        $('#red').fadeIn('slow');
+        $('#tree').fadeIn('slow');
+    });
+
 });
